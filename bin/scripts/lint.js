13cc90c85d48aafd45c99f67e7967b4f18734db3
Eser Ozvataf <eser@ozvataf.com>
2018-10-29 04:51:59 +0300

    Fixed linter config path.

--- bin/scripts/lint.js
+++ bin/scripts/lint.js
@@ -2,7 +2,7 @@ const shellSpawn = require('../utils/shellSpawn');
 const path = require('path');
 
 function lint() {
-    const eslintConfigPath = path.resolve(__dirname, '../../core/etc/eslint.config.json');
+    const eslintConfigPath = path.resolve(__dirname, '../../core/etc/eslint.config.js');
 
     shellSpawn('./node_modules/.bin/eslint', [ '--config', eslintConfigPath, '--ext', '.js,.jsx,.ts,.tsx,.mjs', './src/' ]);
 }
