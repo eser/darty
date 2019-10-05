e4ac5d6d1000cc734d6471b9b56a1ac86dbfe86d
Eser Ozvataf <eser@ozvataf.com>
2019-10-05 23:31:56 +0300

    Added test:coverage command.

---
+++ bin/scripts/test-coverage.js
@@ -0,0 +1,14 @@
+const shellSpawn = require('../utils/shellSpawn');
+const pathFinder = require('../../core/etc/scripts/pathFinder');
+const path = require('path');
+
+function test() {
+    const jestDefaultConfigPath = path.resolve(__dirname, '../../core/etc/jest.config.js');
+    const jestLocalConfigPath = './jest.config.js';
+
+    const jestConfigPath = pathFinder(jestLocalConfigPath, jestDefaultConfigPath);
+
+    shellSpawn('./node_modules/.bin/jest', [ '--config', jestConfigPath, '--rootDir', './', '--coverage' ]);
+}
+
+module.exports = test;
