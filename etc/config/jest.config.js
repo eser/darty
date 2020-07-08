75f507ebf342ff59727e09ea36186e6b4c0af5eb
Eser Ozvataf <eser@ozvataf.com>
2020-07-08 03:51:26 +0300

    Fixed fileTransformer's path.

--- etc/config/jest.config.js
+++ etc/config/jest.config.js
@@ -35,7 +35,7 @@ module.exports = {
     },
     transform: {
         '^.+\\.tsx?$': 'ts-jest',
-        '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': `${__dirname}/../src/utils/fileTransformer.js`,
+        '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': `${__dirname}/fileTransformer.js`,
     },
     collectCoverageFrom: [
         '**/*.{ts,tsx,js,jsx,json}',
