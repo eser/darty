daebee1de26b7427109f97f1eadf3b242e4857e5
Eser Ozvataf <eser@ozvataf.com>
2020-09-05 18:34:13 +0300

    Bumped to version 0.53.6.

--- jest.config.js
+++ jest.config.js
@@ -6,7 +6,7 @@ const jestConfig = {
     transform: {
         '^.+\\.tsx?$': 'ts-jest',
     },
-    testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.([jt]sx?|mjs)$',
+    testRegex: '(/__tests__/.*|.+\\.(test|spec))\\.([jt]sx?|mjs)$',
     moduleFileExtensions: [
         'ts',
         'tsx',
---
+++ src/__tests__/dummy.js
@@ -0,0 +1,7 @@
+describe('placeholder', () => {
+    test('dummy', () => {
+        const result = 1;
+
+        expect(result).toBe(1);
+    });
+});
